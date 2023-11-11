94ecaec414db14ee9dc20be3b1dcff56af17e916
Enric Cosme Llópez <67464597+encosllo@users.noreply.github.com>
2023-11-11 22:42:50 +0100

    S6

--- TallerLean4/S6.lean
+++ TallerLean4/S6.lean
@@ -138,19 +138,17 @@ def suma : N → N → N := by
   | z   => exact m
   | s n => exact s (suma n m)
 
--- La suma també es pot definir per recursió sobre m (recursió a dreta)
-def sumar : N → N → N
-  | n, z => n
-  | n, s m => s (sumar n m)
-
--- Demostreu que les aplicacions coincideixen (Doble inducció)
-theorem TSumar : ∀(n m : N), suma n m = sumar n m := by
-  sorry
+-- Podem emprar notació d'infixe en compte de notació polaca
+-- El número 65 expressa la urgència en avaluar el símbol
+notation : 65 lhs:65 " + " rhs:66 => suma lhs rhs
 
 -- Ara podem demostrar propietats bàsiques sobre la suma
+theorem TRussell : uno + uno = dos := by
+  exact rfl
+
 -- 0 és neutre per l'esquerra per a la suma
 -- Sense inducció
-theorem TSuma0NE : ∀(n:N), suma z n = n := by
+theorem TSuma0NE : ∀(n:N), z + n = n := by
   intro n
   cases n with
   | z => exact rfl
@@ -158,45 +156,132 @@ theorem TSuma0NE : ∀(n:N), suma z n = n := by
 
 -- 0 és neutre per la dreta per a la suma
 -- Amb inducció
-theorem TSuma0ND : ∀(n:N), suma n z = n := by
+theorem TSuma0ND : ∀(n:N), n + z = n := by
   -----------
   -- Cas base
-  have hCB : suma z z = z := by exact rfl
+  have hCB : z + z = z := by exact rfl
   ---------------
   -- Pas inductiu
-  have hInd (n : N) (hi : suma n z = n) : suma (s n) z = s n := by
+  have hInd (n : N) (hi : n + z = n) : (s n) + z = s n := by
     calc
-      suma (s n) z = s (suma n z) := by exact rfl
-      _            = s n          := by exact congrArg s hi
+      (s n) + z = s (n + z) := by exact rfl
+      _         = s n       := by exact congrArg s hi
   -- Demostració per inducció
   intro n
   induction n with
   | z => exact hCB
   | s n hi => exact hInd n hi
 
--- La suma és commutativa
-theorem TSumaComm : ∀(n m : N), suma n m = suma m n := by
+-- Ara demostrarem que la suma commuta
+-- Abans, necessitem un lema previ
+theorem TSumUn : ∀ (n m : N), (s n) + m = n + (s m) := by
   -----------
   -- Cas base
-  have hCB : ∀ (m:N), suma z m = suma m z := by
+  have hCB : ∀ (m:N), (s z) + m = z + (s m) := by
     intro m
     calc
-      suma z m  = m         := by exact rfl
-      _         = suma m z  := by exact (TSuma0ND m).symm
+      (s z) + m = s (z + m) := by exact rfl
+      _ = s m := by exact rfl
+      _ = z + (s m) := by exact rfl
   ---------------
   -- Pas inductiu
-  have hInd (n m : N) (hi : suma n m = suma m n) : suma (s n) m = suma m (s n) := by
+  have hInd (n m : N) (hi : (s n) + m = n + (s m)) : (s (s n)) + m = (s n) + (s m) := by
     calc
-      suma (s n) m  = s (suma n m)  := by exact rfl
-      _             = s (suma m n)  := by exact congrArg s hi
-      _             = s (sumar m n) := by exact congrArg s (TSumar m n)
-      _             = sumar m (s n) := by exact rfl
-      _             = suma m (s n)  := by exact (TSumar m (s n)).symm
+      (s (s n)) + m = s ((s n) + m) := by exact rfl
+      _ = s (n + (s m)) := by exact congrArg s hi
+      _ = (s n) + (s m) := by exact rfl
+  intro n m
+  induction n with
+  | z => exact hCB m
+  | s n hi => exact hInd n m hi
+
+-- La suma és commutativa
+theorem TSumaComm : ∀(n m : N), n + m = m + n := by
+  -----------
+  -- Cas base
+  have hCB : ∀ (m:N), z + m = m + z := by
+    intro m
+    calc
+      z + m  = m      := by exact rfl
+      _      = m + z  := by exact (TSuma0ND m).symm
+  ---------------
+  -- Pas inductiu
+  have hInd (n m : N) (hi : n + m = m + n) : (s n) + m = m + (s n) := by
+    calc
+      (s n) + m  = s (n + m)  := by exact rfl
+      _             = s (m + n)  := by exact congrArg s hi
+      _             = (s m) + n  := by exact rfl
+      _             = m + (s n)  := by exact TSumUn m n
   -- Demostració per inducció
   intro n m
   induction n with
   | z => exact hCB m
   | s n hi => exact hInd n m hi
 
+-- La suma és associativa
+theorem TSumaAss : ∀(n m p : N), (n + m) + p = n + (m + p) := by
+  -----------
+  -- Cas base
+  have hCB : ∀ (n m : N), (n + m) + z = n + (m + z) := by
+    intro n m
+    calc
+      (n + m) + z = n + m := by exact TSuma0ND (suma n m)
+      _ = n + (m + z) := by exact congrArg (suma n) (id (TSuma0ND m).symm)
+  ---------------
+  -- Pas inductiu
+  have hInd (n m p: N) (hi : (n + m) + p = n + (m + p)) : (n + m) + (s p) = n + (m + (s p)) := by
+    calc
+      (n + m) + (s p) = (s (n + m)) + p := by exact (TSumUn (suma n m) p).symm
+      _ = s ((n + m) + p) := by exact rfl
+      _ = s (n + (m + p)) := by exact congrArg s hi
+      _ = (s n) + (m + p) := by exact rfl
+      _ = n + (s (m + p)) := by exact TSumUn n (suma m p)
+      _ = n + ((s m) + p) := by exact rfl
+      _ = n + (m + (s p)) := by exact congrArg (suma n) (TSumUn m p)
+  intro n m p
+  induction p with
+  | z => exact hCB n m
+  | s p hi => exact hInd n m p hi
 
 end Suma
+
+namespace Producte
+open Suma
+-- Anem a definir el producte de naturals de forma recursiva
+-- Notem que fem recursió sobre n (recursió a esquerra)
+def prod : N → N → N := by
+  intro n m
+  cases n with
+  | z   => exact z
+  | s n => exact (prod n m) + m
+
+-- Podem emprar notació d'infixe en compte de notació polaca
+-- El número 70 expressa la urgència en avaluar el símbol
+-- Com 70 és major que 65, primer s'avaluarà el producte i després la suma
+notation : 70 lhs:70 " * " rhs:71 => prod lhs rhs
+
+-- Ara podem demostrar propietats bàsiques sobre el producte
+theorem TRussell : uno * uno = uno := by
+  exact rfl
+
+-- 0 és aniquilador per l'esquerra per al producte
+theorem TProd0AE : ∀(n:N), z * n = z := by
+  sorry
+
+-- 1 és neutre per l'esquerra per al producte
+theorem TProd1NE : ∀(n:N), uno * n = n := by
+  sorry
+
+-- El productes és communtatiu
+theorem TProdComm : ∀(n m : N), n * m = m * n := by
+  sorry
+
+-- El producte és associatiu
+theorem TProdAss : ∀(n m p : N), (n * m) * p = n * (m * p) := by
+  sorry
+
+-- El producte distribueix per l'esquerra
+theorem TProdDis : ∀(n m p : N), n * (m + p) = n * m + n * p := by
+  sorry
+
+end Producte
