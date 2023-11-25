59234ba397a62cd4dad5f7c32f603eb057dda755
Enric Cosme Llópez <67464597+encosllo@users.noreply.github.com>
2023-11-25 21:38:14 +0100

    S8

--- TallerLean4/S8.lean
+++ TallerLean4/S8.lean
@@ -96,22 +96,109 @@ theorem LOrd4 (n m : N) (h1 : n ≼ m) : s n ≼ s m := by
   exact congrArg s h1
 
 -- Lema 5
-theorem LOrd5 (n m k : N) : (it Dprec k n m) → (it Dprec k m n) → k = z := by
-  intro h1 h2
-  -- Per inducció sobre k
-  induction k
-  -- Cas base
+theorem LOrd5 (n : N) : z ≤ n := by
+  use n
+  induction n
   exact rfl
-  -- Pas inductiu
-  rename_i p hInd
-  cases h1
+  rename_i n hInd
+  apply Or.inr
+  use n
+  apply And.intro
+  exact hInd
+  exact rfl
+
+-- Lema 6
+theorem LOrd6 (n m k : N) : it Dprec k n m ↔ it Dprec k (s n) (s m) := by
+  apply Iff.intro
+  intro h
+  induction k
+  exact congrArg s h
+  rename_i k hInd
+  cases h
+  rename_i hinl
+  exact Or.inl (hInd hinl)
+  rename_i hinr
+  apply Exists.elim hinr
+  intro p
+  intro ⟨h2, h3⟩
+  apply Or.inr
+  use p
   sorry
+
+-- Lema 7
+theorem LOrd7 (n k : N) : (it Dprec k n z) → n = z := by
+  intro h1
+  induction k
+  exact h1
+  rename_i k hInd
+  cases h1
+  --
+  rename_i h1inl
+  exact hInd h1inl
+  --
+  rename_i h1inr
+  apply Exists.elim h1inr
+  intro p
+  intro ⟨h1,h2⟩
+  rw [Dprec] at h2
+  injection h2
+
+-- Lema 8
+theorem LOrd8 (n m k : N) (h1 : it Dprec k (s n) (s m)) : it Dprec k n m := by
+  induction k
+  injection h1
+  rename_i k hInd
+  cases h1
+  rename_i h1
+  have h2 : it Dprec k n m := by exact hInd h1
+  exact Or.inl h2
+  rename_i h1
+  apply Exists.elim h1
+  intro p
+  intro ⟨h3,h4⟩
+  rw [Dprec] at h4
+  injection h4 with h4
+  rw [h4.symm] at h3
+  sorry
+
+-- Lema 9
+theorem LOrd9 (n k : N) : ¬(it Dprec k (s n) n) := by
+  by_contra h1
+  induction n
+  induction k
+  injection h1
+  rename_i n hInd
+  have h2 : s z = z := by exact LOrd7 (s z) (s n) h1
+  injection h2
+  rename_i n hInd
+  have h2: it Dprec k (s n) n := by exact LOrd8 (s n) n k h1
+  exact hInd h2
 
 open Suma
 #check TSumaComm
 
--- Lema 6
-theorem LOrd6 (n m : N) : (n ≤ m) → (m ≤ n) → n = m := by
+-- Lema 10
+theorem LOrd10 (n m k : N) : (it Dprec k n m) → (it Dprec k m n) → n = m := by
+  intro h1 h2
+  -- Per inducció sobre k
+  induction k
+  exact h1
+  rename_i k hInd
+  cases h1
+  rename_i h1inl
+  cases h2
+  rename_i h2inl
+  exact hInd h1inl h2inl
+  rename_i h2inr
+  apply Exists.elim h2inr
+  intro p
+  intro ⟨h3, h4⟩
+  rw [Dprec] at h4
+  rw [h4] at h1inl
+  sorry
+
+-- Lema 11
+theorem LOrd11 (n m : N) : (n ≤ m) → (m ≤ n) → n = m := by
   intro h1 h2
   apply Exists.elim h1
   intro k h3
@@ -126,16 +213,11 @@ theorem LOrd6 (n m : N) : (n ≤ m) → (m ≤ n) → n = m := by
   have h7 : k + l = l + k := by
     apply TSumaComm
   rw [h7] at h5
-  have h8 : l+k = z := by
-    exact LOrd5 n m (l+k) h5 h6
-  have h9 : l = z := by
-    exact TSumaz l k h8
-  rw [h9] at h4
-  exact h4.symm
+  exact LOrd10 n m (l + k) h5 h6
 
 -- La relació ≤ és antisimètrica
 theorem DleqAnti : antisimetrica Dleq := by
   rw [antisimetrica]
   intro n m
   intro ⟨h1, h2⟩
-  exact LOrd6 n m h1 h2
+  exact LOrd11 n m h1 h2
